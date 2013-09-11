c16294e6325ac3b495b7f63d297021db6e3441cf
Nick Jones <nick.jones@meanbee.com>
2013-09-11 08:41:54 +0100

    Add basic diff command

--- src/MageConfigSync/Application.php
+++ src/MageConfigSync/Application.php
@@ -3,6 +3,7 @@
 namespace MageConfigSync;
 
 use MageConfigSync\Command\DumpCommand;
+use MageConfigSync\Command\DiffCommand;
 
 class Application extends \Symfony\Component\Console\Application
 {
@@ -11,5 +12,6 @@ class Application extends \Symfony\Component\Console\Application
         parent::__construct();
 
         $this->add(new DumpCommand());
+        $this->add(new DiffCommand());
     }
 }
---
+++ src/MageConfigSync/Command/DiffCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace MageConfigSync\Command;
+
+use MageConfigSync\ConfigYaml;
+use MageConfigSync\Magento;
+use MageConfigSync\Magento\ConfigurationAdapter;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Parser;
+
+class DiffCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('diff')
+            ->setDescription('Compare the current Magento configuration with a file.')
+            ->addArgument(
+                'config-yaml-file',
+                InputArgument::REQUIRED,
+                'The YAML file containing the configuration settings.'
+            )
+            ->addOption(
+                'magento-root',
+                null,
+                InputArgument::OPTIONAL,
+                'The Magento root directory, defaults to current working directory.',
+                getcwd()
+            )
+            ->addOption(
+                'file-env',
+                null,
+                InputArgument::OPTIONAL,
+                'Environment used in the YAML file.  If one is not provided, no environment will be used.'
+            )
+            ->addOption(
+                'db-env',
+                null,
+                InputArgument::OPTIONAL,
+                'Environment used in the database.  If one is not provided, no environment will be used.'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $magento = new Magento($input->getOption('magento-root'));
+        $config_adapter = new ConfigurationAdapter($magento);
+
+        $yaml = new Parser();
+
+        if ($input->getArgument('config-yaml-file')) {
+            $config_yaml_file = $input->getArgument('config-yaml-file');
+
+            if (!file_exists($config_yaml_file)) {
+                throw new \Exception("File ($config_yaml_file) does not exist");
+            }
+
+            if (!is_readable($config_yaml_file)) {
+                throw new \Exception("File ($config_yaml_file) is not readable");
+            }
+
+            $config_db_yaml = ConfigYaml::build($config_adapter, $input->getOption('db-env'));
+
+            $config_file_data = $yaml->parse(file_get_contents($config_yaml_file));
+            $config_file_yaml = new ConfigYaml($config_file_data, $input->getOption('file-env'));
+
+            $diff = ConfigYaml::compare($config_file_yaml, $config_db_yaml);
+
+            if (count($diff) > 0) {
+                foreach ($diff as $scope => $scope_data) {
+                    foreach ($scope_data as $key => $value) {
+                        printf("%s/%s is different\n", $scope, $key);
+                    }
+                }
+            }
+        }
+    }
+}
