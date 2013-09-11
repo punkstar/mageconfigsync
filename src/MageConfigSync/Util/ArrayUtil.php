2890de2a426949d23bcc212105bd2a0a7538351f
Nick Jones <nick.jones@meanbee.com>
2013-09-11 08:40:50 +0100

    Add an implementation of array_diff_assoc_recursive

---
+++ src/MageConfigSync/Util/ArrayUtil.php
@@ -0,0 +1,31 @@
+<?php
+namespace MageConfigSync\Util;
+
+class ArrayUtil
+{
+    /**
+     * http://www.php.net/manual/en/function.array-diff-assoc.php#111675
+     *
+     * @param $array1
+     * @param $array2
+     * @return array
+     */
+    public static function diff_assoc_recursive($array1, $array2)
+    {
+        $difference=array();
+        foreach($array1 as $key => $value) {
+            if( is_array($value) ) {
+                if( !isset($array2[$key]) || !is_array($array2[$key]) ) {
+                    $difference[$key] = $value;
+                } else {
+                    $new_diff = self::diff_assoc_recursive($value, $array2[$key]);
+                    if( !empty($new_diff) )
+                        $difference[$key] = $new_diff;
+                }
+            } else if( !array_key_exists($key,$array2) || $array2[$key] !== $value ) {
+                $difference[$key] = $value;
+            }
+        }
+        return $difference;
+    }
+}
