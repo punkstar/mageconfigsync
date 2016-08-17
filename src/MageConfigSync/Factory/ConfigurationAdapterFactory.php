e73da9de55c07f11e8802f5ee56ce8069e5413b1
Nathan McBride <nathan@brideo.co.uk>
2016-08-17 14:27:21 +0100

    Create factory methods for handling `ConfigurationAdapter`
    So we can easily switch our implementation from Magento1 or Magento2
    we should allow a factory to create our `$config_adapter`.

--- src/MageConfigSync/Factory/ConfigurationAdapterFactory.php
+++ src/MageConfigSync/Factory/ConfigurationAdapterFactory.php
@@ -1,1 +1,51 @@
 <?php
+
+namespace MageConfigSync\Factory;
+
+use MageConfigSync\Api\ConfigurationAdapterInterface;
+use MageConfigSync\Magento;
+use MageConfigSync\Magento2;
+use MageConfigSync\Magento2\ConfigurationAdapter as Magento2Configuration;
+use MageConfigSync\Magento\ConfigurationAdapter as MagentoConfiguration;
+use Symfony\Component\Console\Input\InputInterface;
+
+class ConfigurationAdapterFactory
+{
+
+    /**
+     * @param InputInterface $input
+     *
+     * @return ConfigurationAdapterInterface
+     */
+    static public function create(InputInterface $input)
+    {
+        if ($input->getOption('magento2')) {
+            return static::createMagento2Configuration($input->getOption('magento-root'));
+        }
+
+        return static::createMagentoConfiguration($input->getOption('magento-root'));
+    }
+
+    /**
+     * @param $magento2_root
+     *
+     * @return Magento2Configuration
+     */
+    static public function createMagento2Configuration($magento2_root)
+    {
+        $magento2 = new Magento2($magento2_root);
+        return new Magento2Configuration($magento2);
+    }
+
+    /**
+     * @param $magento_root
+     *
+     * @return MagentoConfiguration
+     */
+    static public function createMagentoConfiguration($magento_root)
+    {
+        $magento = new Magento($magento_root);
+        return new MagentoConfiguration($magento);
+    }
+
+}
